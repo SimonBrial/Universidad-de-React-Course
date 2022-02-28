8d1cf62c94af6d122c4625938dfa35f16c49cd5f
SimonBrial <53979704+SimonBrial@users.noreply.github.com>
2022-02-28 16:43:30 -0400

    Add files via upload
    Parte 2: segunda parte de los archivos del curso

---
+++ 12) Animaciones y transiciones en React/3) Workshop de componenete Accordion 2.0.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Acordion from './components/Acordion'
+
+const App = () => {
+  return (
+    <div>
+      <Acordion
+        title='Ejemplo de Acordion'
+        content='Lorem ipsum...'
+        bgColor='#000'
+      />
+      <Acordion
+        title='Porque Universidad React'
+        content='El curso mas completo y actualizado de React ✪, aprenderás desde nivel cero hasta conocer y dominar no solo React si no su core y podrás crear componentes de calidad de producción.'
+      />
+      <Acordion
+        title='Ejemplo de Acordion'
+        content='Lorem ipsum...'
+        bgColor='orangered'
+      />
+    </div>
+  )
+}
+
+export default App
